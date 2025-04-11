5ec6db9e2ed608b1ed272500c68cdac7deeab0fc
bekkel15 <rexup15@gmail.com>
2025-04-11 10:50:45 +0300

    hw-11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,36 +1,38 @@
-import axios from "axios";
+import axios from 'axios';
+
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-const myApiKey = `49705532-1e2ede49dc369f58ee6a018a1`;
+const myApiKey = '49354290-35d6e1dc5d842ed86975730ff';
 const axiosOptions = key => {
-    const params = {
+  const params = {
     params: {
-        key: myApiKey,
-        q: key,
-        image_type: `photo`,
-        orientation: `horizontal`,
-        safesearch: true,
-        per_page: 40,
+      key: myApiKey,
+      q: key,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
     },
-    };
-    return params;
-} 
+  };
+  return params;
+};
+
 export function apiRequest(requestKey) {
-    return new Promise((resolve, reject) => {
-        axios
-        .get('', axiosOptions(requestKey))
-        .then(response => {
-            if (response.data.total !== 0) {
-                resolve(response.data.hits);
-                return;
-            } else{
-                reject(
-                    `Sorry, there are no images matching your ${requestKey}. Please try again!`
-                );
-                return;
-            }
-        })
-        .catch(response => {
-            reject(response.massage);
-        });
-    });
-} 
+  return new Promise((resolve, reject) => {
+    axios
+      .get('', axiosOptions(requestKey))
+      .then(response => {
+        if (response.data.total !== 0) {
+          resolve(response.data.hits);
+          return;
+        } else {
+          reject(
+            `Sorry, there are no images matching your ${requestKey}. Please try again!`
+          );
+          return;
+        }
+      })
+      .catch(response => {
+        reject(response.message);
+      });
+  });
+}
